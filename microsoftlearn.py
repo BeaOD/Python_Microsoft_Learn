4a0886eb99f76cf63822af88159a4ba73a5be1f2
Bea_OD <owusubea@gmail.com>
2025-09-15 11:05:52 +0000

    first commit

---
+++ microsoftlearn.py
@@ -0,0 +1,31 @@
+# pi = 3.14159
+# print(pi)
+
+# first_num = input('enter a number')
+# second_num = input('enter a number')
+# print(int(first_num) + float(second_num))
+
+# days_in_february = 28
+# print(str(days_in_february) + "" + "total days in february")
+
+from datetime import datetime, timedelta
+
+currentDate = datetime.now()
+# print('Today is: ' + str(currentDate))
+
+# oneDay = timedelta(days=1)
+# yesterday = currentDate - oneDay
+# print('yesterday was ' + str(yesterday)) 
+
+# lastWeek = timedelta(weeks=1)
+# last_week = currentDate - lastWeek
+# print('last week was' + str(last_week))
+
+# print ('Day: ' + str(currentDate.day))
+# print ('Month: ' + str(currentDate.month))
+# print ('Year: ' + str(currentDate.year))
+
+# NOTE: convert a date inputed as string to date time object
+birthday = input('when is your birthday (dd/mm/yyyy)?')
+birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
+print ("My birthday: " + str(birthday_date))
