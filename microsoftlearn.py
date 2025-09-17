ba8227cb7da2f6cf165d5b1abc6b094da6fa26b1
BeaOD <owusubea@gmail.com>
2025-09-17 23:47:08 +0000

    errors

--- microsoftlearn.py
+++ microsoftlearn.py
@@ -9,9 +9,9 @@
 # print(str(days_in_february) + "" + "total days in february")
 
 
-num = 45
-num_1 = 8
-print(num_1 ** 45)
+# num = 45
+# num_1 = 8
+# print(num_1 ** 45)
 
 from datetime import datetime, timedelta
 
@@ -26,12 +26,17 @@ currentDate = datetime.now()
 # last_week = currentDate - lastWeek
 # print('last week was' + str(last_week))
 
-print ('Day: ' + str(currentDate.day))
-print ('Month: ' + str(currentDate.month))
-print ('Year: ' + str(currentDate.year))
+# print ('Day: ' + str(currentDate.day))
+# print ('Month: ' + str(currentDate.month))
+# print ('Year: ' + str(currentDate.year))
 
 # NOTE: convert a date inputed as string to date time object
-birthday = input('when is your birthday (dd/mm/yyyy)?')
-birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
-print ("My birthday: " + str(birthday_date))
-print ("My birthday is on:" + str(birthday_date))
+# birthday = input('when is your birthday (dd/mm/yyyy)?')
+# birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
+# print ("My birthday: " + str(birthday_date))
+# print ("My birthday is on:" + str(birthday_date))
+
+x=42
+y=54
+if x !=y:
+    print("success")
